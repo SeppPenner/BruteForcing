45002e1b0346253ea5039b286e5aa241c118d29a
SeppPenner <HansM2013@gmx.de>
2022-02-21 16:36:28 +0100

    Updated build script.

--- PushPackage.bat
+++
@@ -1,3 +0,0 @@
-dotnet nuget push "src\BruteForcing\bin\Release\HaemmerElectronics.SeppPenner.BruteForcing.*.nupkg" -s "github" --skip-duplicate
-dotnet nuget push "src\BruteForcing\bin\Release\HaemmerElectronics.SeppPenner.BruteForcing.*.nupkg" -s "nuget.org" --skip-duplicate -k "%NUGET_API_KEY%"
-PAUSE
